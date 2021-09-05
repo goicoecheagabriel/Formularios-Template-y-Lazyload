6460e8a01a8752ad6d00e649affc31873c63c152
Iñaki <goicoecheagabriel@gmail.com>
2021-09-05 15:46:47 +0200

    Aplicacion con estructura minima funcionando. Un componente shared con el sidemenu y dos mdules, reactive y template, cada uno con su router para ser configurado con lazyload en el app-routing-module.ts

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forChild(routes)
+  ],
 })
-export class AppRoutingModule { }
+export class TemplateRoutingModule { }
